f73ad901ac6acaba89982b2cd6ba34d9b53fc9be
Luke Weston <lukeweston1234@gmail.com>
2022-09-10 23:20:15 -0400

    css changes

--- src/components/MiniScreen.jsx
+++ src/components/MiniScreen.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/MiniScreen.css";
 
 const MiniScreen = ({ activeButton }) => {
-  return <div className="mini-screen">{activeButton}</div>;
+  return (
+    <div className="mini-screen">
+      {`${activeButton[0].toUpperCase() + activeButton.slice(1)}`}
+    </div>
+  );
 };
 
 export { MiniScreen };
